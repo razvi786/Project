307ca60fb90c7991fa66efd79b7d7b1ce6681460
razvi786 <ehsan.razvi92@gmail.com>
2020-01-27 17:55:50 +0530

    Day 22 Project added router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginFormComponent},
+  {path:'register',component:RegisterFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
